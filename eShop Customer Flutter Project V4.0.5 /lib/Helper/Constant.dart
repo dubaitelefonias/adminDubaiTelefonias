c92daf1a6ef9cd2a6441f212434847abd1b92b47
Santiago Guido <santiagoguido@live.com>
2023-09-03 22:36:37 -0600

    nueva subida

---
+++ eShop Customer Flutter Project V4.0.5 /lib/Helper/Constant.dart
@@ -0,0 +1,25 @@
+const String appName = 'dubaitelefonia';
+
+const String packageName = 'com.dubaitelefonia.customer';
+const String androidLink = 'https://play.google.com/store/apps/details?id=';
+
+const String iosPackage = 'com.dubaitelefonia.customer';
+const String iosLink = 'your ios link here';
+const String appStoreId = '123456789';
+
+const String deepLinkUrlPrefix = 'https://eshopwrteamin.page.link';
+const String deepLinkName = 'eshop.com';
+
+const int timeOut = 50;
+const int perPage = 10;
+
+//Token ExpireTime in minutes & issuer name
+const int tokenExpireTime = 5;
+const String issuerName = 'eshop';
+
+const String baseUrl = 'PLACE_HERE_YOUR_CUSTOMER_APP_URL';
+const String jwtKey = "PLACE_HERE_YOUR_JWT_KEY";
+
+const String defaultCountryCode = 'MX';
+
+const int decimalPoints = 2;
---
+++ eShop DeliveryBoy Flutter Project V4.0.5/android/app/src/main/kotlin/com/eshopsingle/deliveryboy/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.dubaitelefonia.deliveryboy
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
